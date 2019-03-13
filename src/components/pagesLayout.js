e1b7c62f12090dd6f02e6c2dbe452aacc3948d05
Bobby Lee <rdlee8181@gmail.com>
2019-03-12 23:05:57 -0700

    Updated location of Featured img on posts

--- src/components/pagesLayout.js
+++ src/components/pagesLayout.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react"
 import { graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "./layout"
+import SEO from "./seo"
 
 export default class Page extends Component {
   render() {
--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -2,25 +2,30 @@ import React, { Component } from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "./layout"
+import SEO from "./seo"
 
 export default class Post extends Component {
   render() {
     const { data } = this.props
+
     return (
       <Layout>
         <SEO title="Posts" />
+        <p>{img}</p>
+        {data.wordpressPost.featured_media !== null ? (
+          <Img
+            fluid={
+              data.wordpressPost.featured_media.localFile.childImageSharp.fluid
+            }
+          />
+        ) : null}
+
         <h1>{data.wordpressPost.title}</h1>
         <div
           dangerouslySetInnerHTML={{
             __html: data.wordpressPost.content,
           }}
-        />
-        <Img
-          fluid={
-            data.wordpressPost.featured_media.localFile.childImageSharp.fluid
-          }
         />
       </Layout>
     )
